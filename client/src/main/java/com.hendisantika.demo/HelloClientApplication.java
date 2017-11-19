f37b340c8aadf7e25f7f9957b48491624a1f1b6d
Hendi Santika <hendisantika@gmail.com>
2017-11-19 10:12:27 +0700

    #5 - Modify to Dalston SR4 version

---
+++ client/src/main/java/com.hendisantika.demo/HelloClientApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.demo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : feign-eureka
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/11/17
+ * Time: 10.09
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloClientApplication {
+}
